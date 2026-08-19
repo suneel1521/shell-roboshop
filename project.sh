6d3f6052676a1f2d7442689ca1b3f1a717bf5d37
suneel <suneelchalla444@gmail.com>
2026-08-19 14:09:07 +0530

    ok

--- project.sh
+++ project.sh
@@ -1,4 +1,4 @@
-#!/bn/bash
+#!/bin/bash
 AMI_ID="ami-0220d79f3f480ecf5"
 SG_ID="sg-035cf3b369ddee7f8"
 INSTANCES=("frontend" "python" "mysql" "cart" "user")
@@ -7,7 +7,7 @@ for instance in "${INSTANCES[@]}"
 do
   echo "cerating $instance instance"
 
-  INSTANCE_ID=$(aws ec2 run-instances / --image-id "$AMI_ID" /--instance-type t3.micro /--security-group-ids "$SG_ID /--tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}] /--query "Instances[0].InstanceId" /--output "text")
+  INSTANCE_ID=$(aws ec2 run-instances \ --image-id "$AMI_ID" \ --instance-type t3.micro \ --security-group-ids "$SG_ID" \ --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \ --query "Instances[0].InstanceId" \ --output text)
   echo "Instance Id : $INSTANCE_ID"
   if [ "$instance" != "frontend" ]
     then
