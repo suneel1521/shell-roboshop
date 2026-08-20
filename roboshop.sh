a30feb3b4a32681b573d6f5bae18e6953c699d24
suneel <suneelchalla444@gmail.com>
2026-08-20 12:41:43 +0530

    message

--- roboshop.sh
+++ roboshop.sh
@@ -3,7 +3,7 @@ AMI_ID="ami-0220d79f3f480ecf5"
 SG_ID="sg-0284b2fb39b1615a6"
 INSTANCES=("frontend" "catelogue" "mongodb")
 
-for instannce in "${INSTANCE[@]}"
+for instannce in "${INSTANCES[@]}"
 do
     echo "creating $instance instance"
     INSTANCE_ID=$(aws ec2 run-instances \
